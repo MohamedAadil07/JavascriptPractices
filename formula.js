966d4bfc4b4f4daf68f69737e7e10def0ecaf120
MohamedAadil07 <96661818+MohamedAadil07@users.noreply.github.com>
2023-07-10 11:54:08 +0530

    Add files via upload

---
+++ formula.js
@@ -0,0 +1,17 @@
+
+function cube(n1){
+    let res1=n1**3;
+    return res1;
+}
+ let result1=cube(3)
+
+function mul(n2){
+    let res2=3*n2;
+    return res2
+}
+let result2=mul(30)
+
+let a=2;
+
+let result=result1-result2+a;
+console.log(result);
