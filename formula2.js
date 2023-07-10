5bbb82ecd2a0175cbbcc11b1811d9da64c278aee
MohamedAadil07 <96661818+MohamedAadil07@users.noreply.github.com>
2023-07-10 08:24:10 +0000

    formula2

---
+++ formula2.js
@@ -0,0 +1,19 @@
+function mathMul(n1,n2){
+    let x = 2*n1*n2;
+    return x;
+  }
+  
+  let res1=mathMul(3,6);
+  
+  function mathDiv(n1,n2){
+    let y=4/(n1*n2);
+    return y;
+  }
+  
+  let res2=mathDiv(4,8)
+  
+  let z=1;
+  
+  let result= res1+res2+z;
+  
+  console.log(`the final result is ${result}`)
