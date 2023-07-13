37eaf3198d48ee4673990b4dcd68199ae82dbda4
MohamedAadil07 <96661818+MohamedAadil07@users.noreply.github.com>
2023-07-13 03:54:41 +0000

    New file

---
+++ formula4.js
@@ -0,0 +1,10 @@
+//(a**2*b**3)-(a**3+b**2)
+
+let a=prompt('Enter the first value');
+
+let b=prompt('Enter the Second value');
+
+function square_Cube(a,b){
+  return (a**2*b**3)-(a**3+b**2);
+}
+console.log(square_Cube(a,b));
